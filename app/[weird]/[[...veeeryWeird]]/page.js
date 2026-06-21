5425e2b4c9ff29af877ab04e52b8a8e1b5d0f2d3
Gonzalo Arrivi <gonzalo.arrivi@gmail.com>
2026-06-21 12:58:18 +0200

    (wip) upgrading nextjs

--- app/[weird]/[[...veeeryWeird]]/page.js
+++ app/[weird]/[[...veeeryWeird]]/page.js
@@ -1,5 +1,8 @@
 import AwfullyWeird from "./AwfullyWeird";
 
 export default function Page(props) {
-  return <AwfullyWeird {...props} />;
+  return (
+    <AwfullyWeird /* @next-codemod-error 'props' is used with spread syntax (...). Any asynchronous properties of 'props' must be awaited when accessed. */
+    {...props} />
+  );
 }
