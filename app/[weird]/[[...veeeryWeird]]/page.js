eda7a9a9b8fdd2c8c5a6d0b4c4ffb5c52656e6ed
Gonzalo Arrivi <gonzalo.arrivi@gmail.com>
2026-06-21 13:02:07 +0200

    REmove comment

--- app/[weird]/[[...veeeryWeird]]/page.js
+++ app/[weird]/[[...veeeryWeird]]/page.js
@@ -2,7 +2,7 @@ import AwfullyWeird from "./AwfullyWeird";
 
 export default function Page(props) {
   return (
-    <AwfullyWeird /* @next-codemod-error 'props' is used with spread syntax (...). Any asynchronous properties of 'props' must be awaited when accessed. */
+    <AwfullyWeird
     {...props} />
   );
 }
